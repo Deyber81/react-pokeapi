d78498ec7457aac1914e60053a847e79021d8163
Deyber81 <191202@unamba.edu.pe>
2025-09-28 16:20:29 -0500

    feat: agregar tipos de Pokémon y mejorar la definición de funciones en pokemonService

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -1,8 +1,29 @@
 import { pokeApi } from "../api/pokeApi";
+import type { Pokemon, PokemonDetail } from "../types/pokemon";
 
-export const getPokemons = async (limit = 20, offset = 0) => {
+export const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<Pokemon[]> => {
   const { data } = await pokeApi.get(
     `/pokemon?limit=${limit}&offset=${offset}`
   );
+  return data.results;
+};
+
+export const getPokemonDetails = async (
+  name: string
+): Promise<PokemonDetail> => {
+  const { data } = await pokeApi.get(`/pokemon/${name.toLowerCase()}`);
   return data;
+};
+
+export const getAllTypes = async (): Promise<Pokemon[]> => {
+  const { data } = await pokeApi.get(`/type`);
+  return data.results;
+};
+
+export const getPokemonsByType = async (type: string): Promise<Pokemon[]> => {
+  const { data } = await pokeApi.get(`/type/${type.toLowerCase()}`);
+  return data.pokemon.map((p: { pokemon: Pokemon }) => p.pokemon);
 };
